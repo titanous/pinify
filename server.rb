217a58b922e3b4df8c5e5242eda4d0442305496e
Jonathan Rudenberg <jonathan@titanous.com>
2011-09-11 20:13:16 -0400

    Move form into server

--- server.rb
+++ server.rb
@@ -29,10 +29,17 @@ class Pinify < Goliath::API
 
   def response(env)
     return [404, {}, ''] if env['PATH_INFO'] == '/favicon.ico'
-    return [500, {}, 'No image provided'] unless image = env['params']['image']
+    return [200, { 'Content-Type' => 'text/html' }, DATA] unless image = env['params']['image']
 
     result = EM::Synchrony.popen("filter/pinify #{image[:tempfile].path}")
 
     [200, { 'Content-Type' => 'image/jpg' }, result]
   end
 end
+
+__END__
+<title>Pinify</title>
+<form method="POST" enctype="multipart/form-data" action="/">
+  <input name="image" type="file" /><br />
+  <input type="submit" />
+</form>
