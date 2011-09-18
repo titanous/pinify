619b77032674bf9d9999ac46b656a997271a30f4
Jonathan Rudenberg <jonathan@titanous.com>
2011-09-17 20:33:31 -0400

    Refactor.

--- server.rb
+++ server.rb
@@ -1,9 +1,9 @@
 require 'bundler/setup'
 require 'goliath'
 
-TEMPLATE = <<EOS
+UPLOAD_TEMPLATE = <<EOS
 <title>Pinify</title>
-<form method="POST" enctype="multipart/form-data" action="/">
+<form method="POST" enctype="multipart/form-data" action="/upload">
   <input name="image" type="file" /><br />
   <input type="submit" />
 </form>
@@ -36,11 +36,29 @@ class Pinify < Goliath::API
   use Goliath::Rack::Params
 
   def response(env)
-    return [404, {}, ''] if env['PATH_INFO'] == '/favicon.ico'
-    return [200, { 'Content-Type' => 'text/html' }, TEMPLATE] unless image = env['params']['image']
+    case env['PATH_INFO']
+    when '/'
+      render UPLOAD_TEMPLATE
+    when '/upload'
+      process_image env['params']['image']
+    when %r{^/([0-9a-z]+)$}
+    when %r{^/([0-9a-z]+)\.jpg$}
+    else
+      fourohfour
+    end
+  end
 
+  def process_image(image)
+    return fourohfour unless image
     result = EM::Synchrony.popen("filter/pinify #{image[:tempfile].path}")
-
     [200, { 'Content-Type' => 'image/jpg' }, result]
+  end
+
+  def render(content)
+    [200, { 'Content-Type' => 'text/html' }, content]
+  end
+
+  def fourohfour
+    [404, { 'Content-Type' => 'text/html' }, '<h1>404 Not Found</h1>']
   end
 end
