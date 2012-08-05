0b5d9ec3e24ae462290929e8c51ae2587f366b58
Jonathan Rudenberg <jonathan@titanous.com>
2012-08-05 12:31:17 -0400

    Add REDIS_URL

--- app.rb
+++ app.rb
@@ -44,7 +44,7 @@ class Pinify < Sinatra::Base
   helpers do
     def redis
       @redis ||= begin
-        redis_uri = URI.parse(ENV['REDISTOGO_URL'] || 'redis://localhost:6379')
+        redis_uri = URI.parse(ENV['REDISTOGO_URL'] || ENV['REDIS_URL'] || 'redis://localhost:6379')
         Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
       end
     end
